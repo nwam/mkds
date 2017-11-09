77f460deb485a64699bc57847cb3c87b6375af44
Noah Murad <noahmurad@hotmail.com>
2017-11-09 03:49:06 -0500

    Rename PRs view to PR

--- sql/views/PRs.sql
+++
@@ -1,4 +0,0 @@
-SELECT submission_plus.* FROM submission_plus INNER JOIN
-(SELECT MAX(submissionID) as recID FROM submission_plus GROUP BY course, type) as recID
-ON recID.recID = submission_plus.submissionID
-ORDER BY courseID, type;
--- sql/views/submission_plus.sql
+++
@@ -1,8 +0,0 @@
-SELECT q0.submissionID, q0.date AS date, course.courseID as courseID, q0.course AS course, q0.type AS type, q0.time AS time, standard_name.name AS standard, q0.pts AS pts, q0.comment AS comment FROM
-(SELECT submission.submissionID, MAX(submission.date) AS date, submission.course, submission.type, submission.time, MIN(standard.pts) as pts, submission.comment FROM
-(submission JOIN standard ON submission.course = standard.course AND submission.type = standard.type) 
-WHERE submission.time < standard.time
-GROUP BY submission.submissionID, submission.course, submission.type, submission.time, submission.comment) as q0
-JOIN standard_name ON q0.pts = standard_name.pts
-JOIN course ON q0.course = course.course
-ORDER BY submissionID;
